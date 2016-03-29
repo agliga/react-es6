87eb8b32976f53811b82135598599e29dca2c7d8
Andrew Gliga <agliga4@gmail.com>
2016-03-28 18:27:24 -0700

    Fixed main and user routes for es6

--- server/main.js
+++ server/main.js
@@ -1,3 +1,15 @@
 /**
+ *
  * Created by agliga on 3/28/16.
  */
+
+"use strict";
+import UserRoutes from './routes/user';
+import express from 'express';
+
+let app = express();
+
+app.use(UserRoutes);
+app.listen(9010, function () {
+    console.log('Example app listening on port 9010!');
+});
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,3 +1,20 @@
 /**
  * Created by agliga on 3/28/16.
  */
+
+"use strict";
+import express from 'express';
+let router = express.Router();
+
+router.route('/user')
+    .all((req, res, next) => {
+        next();
+    })
+    .get((req, res) => {
+        res.json({
+            userid: 1,
+            username: 'Hello'
+        });
+    });
+
+export default router;

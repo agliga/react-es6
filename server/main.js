c60c87241875506769059b7b09cdf5dcecce3539
Andrew Gliga <agliga4@gmail.com>
2016-03-29 11:42:14 -0700

    Fixed some linting problems

--- server/main.js
+++ server/main.js
@@ -3,13 +3,12 @@
  * Created by agliga on 3/28/16.
  */
 
-"use strict";
 import UserRoutes from './routes/user';
 import express from 'express';
 
-let app = express();
+const app = express();
 
 app.use(UserRoutes);
-app.listen(9010, function () {
-    console.log('Example app listening on port 9010!');
+app.listen(9010, () => {
+  console.log('Example app listening on port 9010!');
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,19 +2,18 @@
  * Created by agliga on 3/28/16.
  */
 
-"use strict";
 import express from 'express';
 let router = express.Router();
 
 router.route('/user')
-    .all((req, res, next) => {
-        next();
-    })
-    .get((req, res) => {
-        res.json({
-            userid: 1,
-            username: 'Hello'
-        });
+  .all((req, res, next) => {
+    next();
+  })
+  .get((req, res) => {
+    res.json({
+      userid: 1,
+      username: 'Hello'
     });
+  });
 
 export default router;
